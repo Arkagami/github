64bd26651c84619de0d0512fa2a80761eb356009
Arkagami <mixno20@gmail.com>
2018-01-17 23:49:38 +0300

    add 4 pintos

---
+++ syscall.c
@@ -0,0 +1,29 @@
+#include "userprog/syscall.h"
+#include <stdio.h>
+#include <syscall-nr.h>
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+
+static void syscall_handler (struct intr_frame *);
+
+void
+syscall_init (void)
+{
+  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+static void
+syscall_handler (struct intr_frame *f UNUSED)
+{
+  if (*(int*) f->esp == SYS_WRITE)
+  {
+    putbuf( ((const char**) f->esp)[2], ((size_t*) f->esp)[3]);
+    return;
+  }
+
+  if (*(int*) f->esp == SYS_EXIT)
+  {
+    exit_status = ((size_t*) f->esp)[1]; printf ("system call!\n");
+    thread_exit ();
+  }
+}
---
+++ syscall.h
@@ -0,0 +1,8 @@
+#ifndef USERPROG_SYSCALL_H
+#define USERPROG_SYSCALL_H
+
+void syscall_init (void);
+
+int exit_status;
+
+#endif /* userprog/syscall.h */
